8bc03e9b0e69752a65b3b78bc52f1ab4d2cf9ebb
Satish Mishra <satishmis9199@gmail.com>
2026-08-25 20:54:40 +0530

    Addded a More comments

--- src/main/java/com/e_commerce/eCommerce/exception/TokenExpiredException.java
+++ src/main/java/com/e_commerce/eCommerce/exception/TokenExpiredException.java
@@ -2,6 +2,6 @@ package com.e_commerce.eCommerce.exception;
 
 public class TokenExpiredException extends RuntimeException {
     public TokenExpiredException() {
-        super("This link has expired");
+        super("This link has expired ..Please Reset again" );
     }
 }
