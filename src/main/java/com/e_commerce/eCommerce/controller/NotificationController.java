38fa03f23fc968a9365d158c185f5ef815b9989c
Satish Mishra <satishmis9199@gmail.com>
2026-09-17 09:57:41 +0530

    live notification feature    b

--- src/main/java/com/e_commerce/eCommerce/controller/NotificationController.java
+++ src/main/java/com/e_commerce/eCommerce/controller/NotificationController.java
@@ -19,7 +19,7 @@ public class NotificationController {
     @GetMapping
     public ResponseEntity<ApiResponse<NotificationListResponse>> getNotifications(
             @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "20") int size,
+            @RequestParam(defaultValue = "10") int size,
             Authentication authentication) {
 
         String tenantId = resolveTenantId(authentication);
